5f639e19d2333a1eca7748efae3935a853bc5a8a
sebaek <sebaek@gmail.com>
2022-10-11 16:12:11 +0900

    greater than

--- src/main/webapp/WEB-INF/sql/08notEquals.sql
+++ src/main/webapp/WEB-INF/sql/08notEquals.sql
@@ -5,6 +5,12 @@ SELECT * FROM Customers WHERE Country != 'Mexico';
 SELECT * FROM Customers WHERE Country != 'Germany';
 
 -- Suppliers 테이블에서 국가가 Japan이 아닌 공급자들 조회
+SELECT * FROM Suppliers WHERE Country <> 'Japan';
+SELECT COUNT(*) FROM Suppliers;
+SELECT COUNT(*) FROM Suppliers WHERE Country <> 'japan';
+SELECT COUNT(*) FROM Suppliers WHERE Country <> 'JAPAN';
+
+SELECT * FROM Employees WHERE EmployeeID <> 1;
 
 
 
---
+++ src/main/webapp/WEB-INF/sql/09greaterThan.sql
@@ -0,0 +1,8 @@
+-- 보다 크다 : >
+SELECT * FROM Customers WHERE CustomerID > 89;
+SELECT * FROM Employees WHERE EmployeeID > 5;
+SELECT * FROM Employees WHERE LastName > 'Callahan';
+SELECT * FROM Employees WHERE BirthDate > '1959-12-31';
+
+-- Products 테이블에서 가격(Price)가 10.00 보다 큰 상품들 조회
+
