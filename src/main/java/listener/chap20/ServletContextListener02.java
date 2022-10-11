4dbdb27f71d3bd74feb859a8ef976ea91fed67b8
sebaek <sebaek@gmail.com>
2022-10-11 11:36:35 +0900

    servletcontextlistener

--- src/main/java/listener/chap20/ServletContextListener02.java
+++ src/main/java/listener/chap20/ServletContextListener02.java
@@ -5,16 +5,16 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 /**
- * Application Lifecycle Listener implementation class ServletContextListener01
+ * Application Lifecycle Listener implementation class ServletContextListener02
  *
  */
 //@WebListener
-public class ServletContextListener01 implements ServletContextListener {
+public class ServletContextListener02 implements ServletContextListener {
 
     /**
      * Default constructor. 
      */
-    public ServletContextListener01() {
+    public ServletContextListener02() {
         // TODO Auto-generated constructor stub
     }
 
@@ -23,17 +23,19 @@ public class ServletContextListener01 implements ServletContextListener {
      */
     public void contextDestroyed(ServletContextEvent sce)  { 
          // TODO Auto-generated method stub
-    	System.out.println("우리 어플리케이션 종료됨!!!");
     }
 
 	/**
      * @see ServletContextListener#contextInitialized(ServletContextEvent)
      */
     public void contextInitialized(ServletContextEvent sce)  { 
-    	System.out.println("우리 어플리케이션 초기화됨!!!!!");
+    	System.out.println("우리 어플리케이션 실행됨22222222222222222");
     }
 	
 }
+
+
+
 
 
 
