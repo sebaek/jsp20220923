9058282d36db26de67d5aaa71de5ba8e467f6a28
sebaek <sebaek@gmail.com>
2022-10-12 09:28:39 +0900

    and

--- src/main/webapp/WEB-INF/sql/11and.sql
+++ src/main/webapp/WEB-INF/sql/11and.sql
@@ -21,3 +21,9 @@ SELECT *
 FROM Customers
 WHERE CustomerName >= 'C'
   AND CustomerName < 'D';
+  
+-- 고객테이블에서 ContactName이 'E'로 시작하는 고객들 조회
+
+-- 상품(Products)테이블에서 가격(Price)가 10.00이상 20.00미만인 상품
+
+-- 직원(Employees)테이블에서 생일이 1950년대생만 조회 (1950-01-01~1959-12-31)
