dc8349d045dfda5787b092118652b4429838c7ea
sebaek <sebaek@gmail.com>
2022-10-12 15:20:20 +0900

    jdbc

---
+++ src/main/java/servlet/chap14/Servlet14.java
@@ -0,0 +1,48 @@
+package servlet.chap14;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet14
+ */
+@WebServlet("/Servlet14")
+public class Servlet14 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet14() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 파라미터 수집
+		// 2. 파라미터 가공
+		// 3. business logic
+		//    db에서 CustomerId가 3번인 고객의 CustomerName을 조회
+		
+		// 4. add attribute 추가 
+		
+		// 5. /WEB-INF/view/chap14/view02.jsp 로 forward
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
