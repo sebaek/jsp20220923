b0c4e94f1adfd3d231b70315140db7c30a734639
sebaek <sebaek@gmail.com>
2022-10-12 16:19:29 +0900

    직원 이름 조회

--- src/main/java/servlet/chap14/Servlet16.java
+++ src/main/java/servlet/chap14/Servlet16.java
@@ -1,7 +1,14 @@
 package servlet.chap14;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,33 +21,58 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Servlet16")
 public class Servlet16 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Servlet16() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Servlet16() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 3. business logic
 		// -> 직원의 LastName을 내림차순 정렬해서 db에서 조회
-		
-		// 4. add attribute
-		
+		String sql = "SELECT LastName FROM Employees ORDER BY 1 DESC";
+
+		ServletContext application = request.getServletContext();
+
+		String url = application.getAttribute("jdbc.url").toString();
+		String user = application.getAttribute("jdbc.username").toString();
+		String pw = application.getAttribute("jdbc.password").toString();
+
+		try (
+				Connection con = DriverManager.getConnection(url, user, pw);
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+
+			List<String> list = new ArrayList<>();
+			while (rs.next()) {
+				list.add(rs.getString(1));
+			}
+
+			// 4. add attribute
+			request.setAttribute("lastNameList", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		// 5. forward/redirect
 		String path = "/WEB-INF/view/chap14/view04.jsp";
 		request.getRequestDispatcher(path).forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
