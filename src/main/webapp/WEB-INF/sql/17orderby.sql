3e4d58a3abff57922dff7e37bc157af82117d609
sebaek <sebaek@gmail.com>
2022-10-12 11:38:55 +0900

    orderby

--- src/main/webapp/WEB-INF/sql/17orderby.sql
+++ src/main/webapp/WEB-INF/sql/17orderby.sql
@@ -36,9 +36,19 @@ SELECT CustomerName, City FROM Customers ORDER BY 1 ASC;
 SELECT CustomerName, City FROM Customers ORDER BY 2, 1;
 
 -- 직원 생일 순으로 직원 조회
+SELECT * FROM Employees ORDER BY BirthDate;
 -- 상품명순으로 상품 조회
+SELECT * FROM Products ORDER BY ProductName;
 -- 가격이 높은 것부터 낮은 것순으로 상품 조회
+SELECT * FROM Products ORDER BY Price DESC;
 -- 공급자를 나라, 도시순으로 조회
+SELECT * FROM Suppliers ORDER BY Country, City;
+
+-- 1번 카테고리에 있는 상품들을 상품명, 가격 조회 (가격 내림차순으로)
+SELECT ProductName, Price
+FROM Products
+WHERE CategoryID = 1
+ORDER BY Price DESC;
 
 
 
