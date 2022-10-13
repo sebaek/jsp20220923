9168f9d066f5926e4204a3b3fe30ec6fd84d2bdd
sebaek <sebaek@gmail.com>
2022-10-13 14:23:31 +0900

    insert into

---
+++ src/main/webapp/WEB-INF/sql/18insert.sql
@@ -0,0 +1,12 @@
+USE w3schools;
+
+SELECT * FROM Employees;
+-- INSERT INTO : 테이블에 레코드 추가하는 명령문
+INSERT INTO Employees 
+(EmployeeID, LastName, FirstName, BirthDate, Photo, Notes)
+VALUES
+(10, 'Son', 'HM', '1999-01-01', 'EmpID10.pic', 'soccer player');
+
+-- 11번째 직원 추가 
+
+
