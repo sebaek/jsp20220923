69e8178230b33b0ec286a256482dad478a510e8d
sebaek <sebaek@gmail.com>
2022-10-13 14:42:31 +0900

    insert into, null

--- src/main/webapp/WEB-INF/sql/18insert.sql
+++ src/main/webapp/WEB-INF/sql/18insert.sql
@@ -1,6 +1,6 @@
 USE w3schools;
 
-SELECT * FROM Employees;
+SELECT * FROM Employees ORDER BY EmployeeID DESC;
 -- INSERT INTO : 테이블에 레코드 추가하는 명령문
 INSERT INTO Employees 
 (EmployeeID, LastName, FirstName, BirthDate, Photo, Notes)
@@ -20,3 +20,25 @@ VALUES
 (12, 'Black', 'Panther', '1970-01-01', 'EmpID12.pic');
 
 -- 13번째 직원 추가, birthDate, notes 컬럼 빼고
+INSERT INTO Employees
+(EmployeeID, LastName, FirstName, Photo)
+VALUES
+(13, 'Doctor', 'Strange', 'EmpID13.pic');
+
+-- 새 직원 추가 , EmployeeID컬럼 빼고
+INSERT INTO Employees
+(LastName, FirstName)
+VALUES
+('Natasha', 'Romanoff');
+
+INSERT INTO Employees
+(LastName, FirstName, Notes)
+VALUES
+('Kim', 'kildong', '');
+
+INSERT INTO Employees
+(LastName, FirstName, Notes)
+VALUES
+('Kim', 'kildong2', ' ');
+
+-- NULL은 값이 없음
