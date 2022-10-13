b3c81a2cf90ca81790dbf3f9fc886e425205fe0a
sebaek <sebaek@gmail.com>
2022-10-13 09:46:05 +0900

    customerName

---
+++ src/main/java/domain/chap14/Customer.java
@@ -0,0 +1,23 @@
+package domain.chap14;
+
+public class Customer {
+	private String name;
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
