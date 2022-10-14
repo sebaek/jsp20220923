467e90bdeb4073be398dbbe9264718f1f13bea25
sebaek <sebaek@gmail.com>
2022-10-14 10:37:35 +0900

    update

--- src/main/webapp/WEB-INF/sql/21update.sql
+++ src/main/webapp/WEB-INF/sql/21update.sql
@@ -14,7 +14,15 @@ DESC Customers;
 SET SQL_SAFE_UPDATES = 0;
 
 -- USA 국가의 고객들의 City 를 New York으로 변경
+UPDATE Customers
+SET City = 'New York'
+WHERE Country = 'USA';
+SELECT * FROM Customers
+WHERE Country = 'USA';
 -- UK 국가의 고객들의 Address를 GangNam으로 변경
-
-
+UPDATE Customers
+SET Address = 'GangNam'
+WHERE Country = 'UK';
+SELECT * FROM Customers
+WHERE Country = 'UK';
 
